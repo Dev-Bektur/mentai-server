a3df18d827088e396d857fba8c521eba3dfb9b29
Bektur <addimilan611@gmail.com>
2026-01-07 21:22:57 +0600

    added roles

--- models/User.js
+++ models/User.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose'); // ВОТ ЭТОЙ СТРОЧКИ НЕ ХВАТАЛО
+const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phone: { type: String },
+  phone: { type: String, required: true },
   password: { type: String, required: true },
+  photo: { type: String },
+  role: { type: String, enum: ['student', 'teacher'], default: 'student' }, // Роль
   mentCoins: { type: Number, default: 0 },
-  rank: { type: String, default: "Новичок" }
+  rank: { type: String, default: 'Новичок' },
+  lessonsPlanned: { type: Array, default: [] }, // Для учителей
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
